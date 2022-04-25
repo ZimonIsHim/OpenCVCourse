bfbab70b3fbb38119fd25459c0336ee537cbadf6
Simon <dutchsimon9@gmail.com>
2022-04-25 14:09:12 +0200

    Add project files.

---
+++ OpenCVCourse/OpdrachtEenPuntTwee.cpp
@@ -0,0 +1,12 @@
+#include <opencv2/imgcodecs.hpp>
+#include <opencv2/highgui.hpp>
+#include <opencv2/imgproc.hpp>
+#include <iostream>
+
+using namespace cv;
+using namespace std;
+
+void main()
+{
+
+}
